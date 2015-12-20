41541f58ad3e837a30f456c7e1a2e65986dba86a
Keigo Suzukawa <k.suzukawa@gmail.com>
2015-12-20 18:27:31 +0000

    Get a handler component and a basic page up

--- project.clj
+++ project.clj
@@ -2,14 +2,22 @@
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.stuartsierra/component "0.3.1"]
+                 [environ "1.0.1"]
                  [bidi "1.22.1"]
                  [http-kit "2.1.18"]
+                 [hiccup "1.0.5"]
+                 [ring "1.4.0"]
+                 [com.taoensso/sente "1.7.0"]
                  [com.taoensso/timbre "4.1.4"]]
 
-  :profiles {:dev {:source-paths ["dev"]
+  :plugins [[lein-environ "1.0.1"]]
+
+  :profiles {:dev {:source-paths ["env/dev"]
 
                    :dependencies [[org.clojure/tools.namespace "0.2.11"]]
 
                    :repl-options {:init-ns structurize.main
                                   :init (structurize.main/start!)
-                                  :port 4000}}})
+                                  :port 4000}
+
+                   :env {:port 3000}}})
--- src/structurize/system.clj
+++ src/structurize/system.clj
@@ -1,34 +1,83 @@
 (ns structurize.system
-  (:require [com.stuartsierra.component :as component]
+  (:require [bidi.ring :as br]
+            [clojure.pprint :as pprint]
+            [com.stuartsierra.component :as component]
+            [environ.core :refer [env]]
+            [ring.util.response :refer [response status content-type]]
+            [hiccup.page :refer [html5]]
+            [taoensso.sente :as sente]
+            [taoensso.sente.server-adapters.http-kit :refer [sente-web-server-adapter]]
             [org.httpkit.server :refer [run-server]]
-            [bidi.ring :as br]
             [taoensso.timbre :refer [info]]))
 
 
-(def routes ["/" {"default" :default}])
+(def page
+  (html5
+   [:head
+    [:title "Structurize"]]
+   [:body
+    [:div
+     [:h1 "Allo World!"]]]))
 
-(def handlers {:default (fn [request] {:status 200 :body "Hello World!"})})
+(def routes ["/" {"a" :a
+                  "b" :b
+                  true :page}])
 
-(defrecord Server [port]
+(def handlers {:a (fn [request] (response "a?"))
+               :b (fn [request] (response "b?"))
+               :page (fn [request] (-> page response (content-type "text/html")))})
+
+
+;;;;;;;;;;;; Handler goods ;;;;;;;;;;;;;;;
+
+(defrecord Handler [config-opts]
   component/Lifecycle
 
   (start [component]
-    (info "Starting server on port" port)
-    (let [handler (br/make-handler routes handlers)
-          stop-server-fn (run-server handler {:port port :join? false})]
-      (assoc component :stop-server stop-server-fn)))
+    (info "Starting handler event loop")
+    (let [handler-fn (br/make-handler routes handlers)]
+      (assoc component :handler-fn handler-fn)))
 
   (stop [component]
-    (when-let [stop-server (:stop-server component)]
-      (info "Stopping server")
-      (stop-server))
+    (info "Stopping handler event loop")
     component))
 
 
-(defn make-server []
-  (map->Server {:port 3000}))
+(defn make-handler [config-opts]
+  (map->Handler {:config-opts config-opts}))
 
 
-(defn make-system []
-  (component/system-map
-   :server (make-server)))
+;;;;;;;;;;;; Server goods ;;;;;;;;;;;;;;;
+
+(defrecord Server [config-opts handler]
+  component/Lifecycle
+
+  (start [component]
+    (info "Starting server on port" (:port config-opts))
+    (let [stop-server-fn (run-server (:handler-fn handler) config-opts)]
+      (assoc component :stop-server-fn stop-server-fn)))
+
+  (stop [component]
+    (when-let [stop-server-fn (:stop-server-fn component)]
+      (info "Stopping server")
+      (stop-server-fn))
+    (dissoc component :stop-server-fn)))
+
+
+(defn make-server [config-opts]
+  (map->Server {:config-opts config-opts}))
+
+
+;;;;;;;;;;;; System goods ;;;;;;;;;;;;;;;
+
+(def system-config-opts
+  {:server-config-opts {:port (env :port)}
+   :handler-config-opts {}})
+
+
+(defn make-system [config-opts]
+  (-> (component/system-map
+        :handler (make-handler (:handler-config-opts config-opts))
+        :server (make-server (:server-config-opts config-opts)))
+      (component/system-using
+        {:server [:handler]})))
