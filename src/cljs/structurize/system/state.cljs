bc08641bec2927b7d467711a95a292516516188f
Keigo Suzukawa <k.suzukawa@gmail.com>
2016-07-13 20:04:35 +0100

    Kill dead code

--- src/cljs/structurize/system/state.cljs
+++ src/cljs/structurize/system/state.cljs
@@ -34,50 +34,6 @@
      (into #{} (concat removed-paths added-paths))))
 
 
-#_(defn make-emit-mutation* [{:keys [config-opts !db]}]
-  (let [log? (get-in config-opts [:general :tooling :log?])
-        tooling-disabled? (not (get-in config-opts [:tooling :enabled?]))
-        max-processed-mutations (get-in config-opts [:tooling :max-processed-mutations])]
-
-    (fn [[id {:keys [Δ] :as props} :as mutation]]
-
-      (let [tooling-mutation? (= (namespace id) "tooling")]
-
-        (when (or (not tooling-mutation?) log?)
-          (log/debug "processing mutation:" id))
-
-        (cond
-          ;; tooling related mutations get swapped in no matter what
-          tooling-mutation? (swap! !db Δ)
-
-          ;; if tooling is disabled, swap in without any mutation decoration
-          tooling-disabled? (swap! !db Δ)
-
-          :else (swap! !db (fn [db]
-                             (let [[_ previous-props] (first (get-in db [:tooling :processed-mutations]))
-                                   pre-Δ-db (:post-Δ-db previous-props (dissoc db :tooling))
-                                   post-Δ-db (Δ pre-Δ-db)
-                                   mutation-paths (make-mutation-paths post-Δ-db pre-Δ-db)
-                                   upstream-mutation-paths (u/upstream-paths mutation-paths)
-                                   updated-props (-> props
-                                                     (assoc :processed-at (t/now)
-                                                            :n (inc (:n previous-props 0))
-                                                            :pre-Δ-db pre-Δ-db
-                                                            :post-Δ-db post-Δ-db
-                                                            :pre-Δ-mutation-paths (:post-Δ-mutation-paths previous-props)
-                                                            :post-Δ-mutation-paths mutation-paths
-                                                            :pre-Δ-upstream-mutation-paths (:post-Δ-upstream-mutation-paths previous-props)
-                                                            :post-Δ-upstream-mutation-paths upstream-mutation-paths))
-                                   updated-mutation [id updated-props]]
-
-                               (-> post-Δ-db
-                                   (assoc :tooling (:tooling db))
-                                   (update-in [:tooling :processed-mutations] (comp (partial take max-processed-mutations) (partial cons updated-mutation)))
-                                   (assoc-in [:tooling :state-browser-props :mutated :paths] mutation-paths)
-                                   (assoc-in [:tooling :state-browser-props :mutated :upstream-paths] upstream-mutation-paths))))))))))
-
-
-
 (defn make-write-app! [config-opts !state]
   (fn [[id f]]
     (let [state @!state
