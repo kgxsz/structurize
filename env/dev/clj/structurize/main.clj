6a5da2026fc595d4cc5930fe4b97ca8924c7c8eb
Keigo Suzukawa <k.suzukawa@gmail.com>
2016-08-28 10:42:43 +0100

    Only reload clojure directories

--- env/dev/clj/structurize/main.clj
+++ env/dev/clj/structurize/main.clj
@@ -1,6 +1,6 @@
 (ns structurize.main
   (:require [structurize.system :as system]
-            [clojure.tools.namespace.repl :refer [refresh refresh-all]]
+            [clojure.tools.namespace.repl :refer [refresh refresh-all set-refresh-dirs]]
             [com.stuartsierra.component :as component]))
 
 (def system (system/make-system))
@@ -13,6 +13,7 @@
 
 (defn reload! [& {:keys [refresh-all?]}]
   (stop!)
+  (set-refresh-dirs "clj")
   (if refresh-all?
     (refresh-all :after 'structurize.main/start!)
     (refresh :after 'structurize.main/start!)))
