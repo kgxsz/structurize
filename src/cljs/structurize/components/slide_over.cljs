c7502a2122e501e519f88a0b6a790c2876ecf81a
Keigo Suzukawa <k.suzukawa@gmail.com>
2016-09-05 21:11:01 +0100

    Model out

--- src/cljs/structurize/components/slide_over.cljs
+++ src/cljs/structurize/components/slide_over.cljs
@@ -12,22 +12,16 @@
   (:require-macros [structurize.components.macros :refer [log-info log-debug log-error]]))
 
 
-;; model ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; types ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn ->+slide-over-open? [+slide-over]
-  (l/*> +slide-over (in [:open?])))
+(s/def ::absolute-width (s/and pos? int?))
+(s/def ::direction #{:top :right :bottom :left})
 
 
 ;; exposed functions ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn toggle-slide-over [x +slide-over]
-  (l/update x (->+slide-over-open? +slide-over) not))
-
-
-;; types ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(s/def ::absolute-width (s/and pos? int?))
-(s/def ::direction #{:top :right :bottom :left})
+  (l/update x +slide-over #(update % :open? not)))
 
 
 ;; components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -36,9 +30,8 @@
   {:pre [(s/valid? ::t/lens +slide-over)
          (s/valid? ::absolute-width absolute-width)
          (s/valid? ::direction direction)]}
-  (let [open? (track φ l/view-single (->+slide-over-open? +slide-over))]
+  (let [{:keys [open?]} (track φ l/view-single +slide-over)]
     (log-debug φ "render slide-over")
     [:div.c-slide-over {:style {:width (str absolute-width "px")
                                 direction (if open? 0 (str (- absolute-width) "px"))}}
      content]))
-
