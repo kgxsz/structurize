7721a156902540b3304788a8adb0830a33fd98db
Keigo Suzukawa <k.suzukawa@gmail.com>
2016-09-05 09:02:43 +0100

    Wrap triptychs in the things they are

--- src/cljs/structurize/components/home_page.cljs
+++ src/cljs/structurize/components/home_page.cljs
@@ -58,7 +58,7 @@
 
 
 (defn masthead-left [Φ {:keys [width col-n col-width gutter margin-left]}]
-  [:div.l-cell.l-cell--justify-end.c-masthead {:style {:width (+ width margin-left)}}
+  [:div.l-cell.l-cell--justify-end {:style {:width (+ width margin-left)}}
    [:div.c-masthead__lip {:style {:width (+ width margin-left)}}]
    [:div.l-cell.l-cell--justify-center {:style {:width col-width}}
     [:div.c-masthead__avatar
@@ -67,7 +67,7 @@
 
 
 (defn masthead-center [Φ {:keys [width col-n col-width gutter margin-left margin-right]}]
-  [:div.l-cell.l-cell--justify-start.c-masthead {:style {:width (+ width margin-left margin-right)}}
+  [:div.l-cell.l-cell--justify-start {:style {:width (+ width margin-left margin-right)}}
    [:div.c-masthead__lip {:style {:width (+ width margin-left margin-right)}}
     [:div.l-cell.l-cell--align-center.l-cell--height-100.c-masthead__primary-content {:style {:padding-left (+ margin-left gutter)
                                                                                               :padding-right (+ margin-right gutter)}}
@@ -120,50 +120,53 @@
                                                   :padding-bottom 6}}
                                     [:div.l-cell.l-cell--fill-parent {:style {:background-color "#F9F9F9"}}]])}}]]
 
-        [triptych Φ {:center {:hidden #{}
-                              :c (fn [Φ {:keys [width col-n col-width gutter margin-left margin-right]}]
-                                   (let [src (rand-nth ["images/hero-1.png" "images/hero-2.png" "images/hero-3.png"
-                                                        "images/hero-4.png" "images/hero-5.png" "images/hero-6.png"
-                                                        "images/hero-7.png" "images/hero-8.png" "images/hero-9.png"])]
-                                     [:div.c-hero {:style {:width (+ width margin-left margin-right)}}
-                                      [image Φ {:+image (in [:home-page :hero-image])
-                                                :src src}]]))}}]
-        [triptych Φ {:left {:hidden #{:xs :sm}
-                            :c masthead-left}
-                     :center {:hidden #{}
-                              :c masthead-center}}]
-        [triptych Φ {:left {:hidden #{:xs :sm}
-                            :c (fn [Φ {:keys [width col-n col-width gutter margin-left]}]
-                                 [:div.l-col.l-col--align-start {:style {:width width
-                                                                         :padding-left gutter
-                                                                         :margin-left margin-left}}
-                                  [triptych-column Φ
-                                   {:width col-width
-                                    :gutter gutter
-                                    :cs [aux-pod aux-pod]}]])}
-                     :center {:hidden #{}
-                              :c (fn [Φ {:keys [width col-n col-width gutter margin-left margin-right]}]
-                                   [:div.l-row.l-row--justify-space-between {:style {:width width
-                                                                                     :padding-left gutter
-                                                                                     :padding-right gutter
-                                                                                     :margin-left margin-left
-                                                                                     :margin-right margin-right}}
-                                    (doall
-                                     (for [i (range col-n)]
-                                       ^{:key i}
-                                       [triptych-column Φ
-                                        {:width col-width
-                                         :gutter gutter
-                                         :cs (repeat 6 content-pod)}]))])}
-                     :right {:hidden #{:xs :sm :md}
-                             :c (fn [Φ {:keys [width col-n col-width gutter margin-right]}]
-                                  [:div.l-col.l-col--align-end {:style {:width width
-                                                                        :padding-right gutter
-                                                                        :margin-right margin-right}}
+        [:div.c-hero
+         [triptych Φ {:center {:hidden #{}
+                               :c (fn [Φ {:keys [width col-n col-width gutter margin-left margin-right]}]
+                                    (let [src (rand-nth ["images/hero-1.png" "images/hero-2.png" "images/hero-3.png"
+                                                         "images/hero-4.png" "images/hero-5.png" "images/hero-6.png"
+                                                         "images/hero-7.png" "images/hero-8.png" "images/hero-9.png"])]
+                                      [:div {:style {:width (+ width margin-left margin-right)}}
+                                       [image Φ {:+image (in [:home-page :hero-image])
+                                                 :src src}]]))}}]]
+        [:div.c-masthead
+         [triptych Φ {:left {:hidden #{:xs :sm}
+                             :c masthead-left}
+                      :center {:hidden #{}
+                               :c masthead-center}}]]
+        [:div.c-page-content
+         [triptych Φ {:left {:hidden #{:xs :sm}
+                             :c (fn [Φ {:keys [width col-n col-width gutter margin-left]}]
+                                  [:div.l-col.l-col--align-start {:style {:width width
+                                                                          :padding-left gutter
+                                                                          :margin-left margin-left}}
                                    [triptych-column Φ
                                     {:width col-width
                                      :gutter gutter
-                                     :cs [aux-pod]}]])}}]
+                                     :cs [aux-pod aux-pod]}]])}
+                      :center {:hidden #{}
+                               :c (fn [Φ {:keys [width col-n col-width gutter margin-left margin-right]}]
+                                    [:div.l-row.l-row--justify-space-between {:style {:width width
+                                                                                      :padding-left gutter
+                                                                                      :padding-right gutter
+                                                                                      :margin-left margin-left
+                                                                                      :margin-right margin-right}}
+                                     (doall
+                                      (for [i (range col-n)]
+                                        ^{:key i}
+                                        [triptych-column Φ
+                                         {:width col-width
+                                          :gutter gutter
+                                          :cs (repeat 6 content-pod)}]))])}
+                      :right {:hidden #{:xs :sm :md}
+                              :c (fn [Φ {:keys [width col-n col-width gutter margin-right]}]
+                                   [:div.l-col.l-col--align-end {:style {:width width
+                                                                         :padding-right gutter
+                                                                         :margin-right margin-right}}
+                                    [triptych-column Φ
+                                     {:width col-width
+                                      :gutter gutter
+                                      :cs [aux-pod]}]])}}]]
 
         #_[:div.l-col.l-col--justify-center
            (if me
