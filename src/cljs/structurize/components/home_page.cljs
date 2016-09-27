092f5d2ccd049c7a0f212eb7bb43787e5092302b
Keigo Suzukawa <k.suzukawa@gmail.com>
2016-09-27 23:25:58 +0100

    Partial clean up of the d3 integration mess

--- src/cljs/structurize/components/home_page.cljs
+++ src/cljs/structurize/components/home_page.cljs
@@ -22,63 +22,63 @@
 
 ;; components ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; TODO - this is a bit all over the place in order to get the svg capturing events
-;; TODO - consider bringing out the SVG all-together into a separate component
-
-(defn voronoi [{:keys [config-opts] :as Φ}]
-  [:svg#voronoi.l-cell.l-cell--fill-parent])
+(defn voronoi [{:keys [config-opts] :as Φ} {:keys [width height]}]
+  (r/create-class
+   {:component-did-mount #(side-effect! Φ :home-page/voronoi-did-mount {:node (r/dom-node %) :width width :height height})
+    ;; TODO re-drawing with newly selected points
+    :component-did-update (fn [_ _] (side-effect! Φ :home-page/voronoi-did-update))
+    :reagent-render (fn []
+                      (let [viewport (track Φ l/view-single
+                                            (in [:viewport]))]
+                        (log-debug Φ "render voronoi")
+                        [:svg.l-cell.l-cell--fill-parent]))}))
 
 (defn home-page [{:keys [config-opts] :as Φ}]
-  (r/create-class
-   {:component-did-mount #(side-effect! Φ :home-page/did-mount {:node (r/dom-node %)})
-    :reagent-render (fn []
-                      (log-debug Φ "render home-page")
-                      (let [me (track Φ l/view-single
-                                      (in [:auth :me]))]
+  (log-debug Φ "render home-page")
+  (let [me (track Φ l/view-single
+                  (in [:auth :me]))
+        {:keys [width height]} (track Φ l/view-single
+                                      (in [:viewport]))]
 
-                        [:div.l-cell.l-cell--height-100.c-home-page
-                         [voronoi Φ]
-                         [:div.l-overlay
-                          [:div.l-col.l-col--width-100.l-col--align-center.l-col--padding-top-25
-                           [:div.l-row
-                            [:span.c-icon.c-icon--diamond.c-icon--h-size-medium.c-icon--margin-right-medium]
-                            [:span.c-text.c-text--h-size-medium "Structurize"]]
+    [:div.l-cell.l-cell--height-100.c-home-page
+     [voronoi Φ {:width width :height height}]
+     [:div.l-overlay
+      [:div.l-col.l-col--width-100.l-col--align-center.l-col--padding-top-25
+       [:div.l-row
+        [:span.c-icon.c-icon--diamond.c-icon--h-size-medium.c-icon--margin-right-medium]
+        [:span.c-text.c-text--h-size-medium "Structurize"]]
 
-                           [:div.l-col.l-col--align-center.l-col--margin-top-xx-large
-                            (let [path (b/path-for (:routes config-opts) :component-guide)]
-                              [:a.c-link.c-link--margin-top-large {:href path
-                                                                   :on-click (u/without-default
-                                                                              #(side-effect! Φ :home-page/change-location
-                                                                                             {:path path}))}
-                               [:span.c-icon.c-icon--layers.c-icon--margin-right-x-small]
-                               [:span.c-text "Component Guide"]])
+       [:div.l-col.l-col--align-center.l-col--margin-top-xx-large
+        (let [path (b/path-for (:routes config-opts) :component-guide)]
+          [:a.c-link.c-link--margin-top-large {:href path
+                                               :on-click (u/without-default
+                                                          #(side-effect! Φ :home-page/change-location
+                                                                         {:path path}))}
+           [:span.c-icon.c-icon--layers.c-icon--margin-right-x-small]
+           [:span.c-text "Component Guide"]])
 
-                            (let [path (b/path-for (:routes config-opts) :store-concept)]
-                              [:a.c-link.c-link--margin-top-large {:href path 
-                                                                   :on-click (u/without-default
-                                                                              #(side-effect! Φ :home-page/change-location
-                                                                                             {:path path}))}
-                               [:span.c-icon.c-icon--crop.c-icon--margin-right-x-small]
-                               [:span.c-text "Design Concepts"]])
+        (let [path (b/path-for (:routes config-opts) :store-concept)]
+          [:a.c-link.c-link--margin-top-large {:href path 
+                                               :on-click (u/without-default
+                                                          #(side-effect! Φ :home-page/change-location
+                                                                         {:path path}))}
+           [:span.c-icon.c-icon--crop.c-icon--margin-right-x-small]
+           [:span.c-text "Design Concepts"]])
 
-                            [:a.c-link.c-link--margin-top-large {:on-click (u/without-propagation
-                                                                            #(side-effect! Φ (if me
-                                                                                               :home-page/sign-out
-                                                                                               :home-page/initialise-sign-in-with-github)))}
-                             [:span.c-icon.c-icon--github.c-icon--margin-right-x-small]
-                             [:span.c-text (if me "Sign out" "Sign in")]]]]]]))}))
+        [:a.c-link.c-link--margin-top-large {:on-click (u/without-propagation
+                                                        #(side-effect! Φ (if me
+                                                                           :home-page/sign-out
+                                                                           :home-page/initialise-sign-in-with-github)))}
+         [:span.c-icon.c-icon--github.c-icon--margin-right-x-small]
+         [:span.c-text (if me "Sign out" "Sign in")]]]]]]))
 
 
 ;; side-effects ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defmethod process-side-effect :home-page/did-mount [Φ id {:keys [node]}]
-  (let [{:keys [height width]} (read Φ l/view-single
-                                     (in [:viewport]))
+(defmethod process-side-effect :home-page/voronoi-did-mount [Φ id {:keys [node width height]}]
+  (let [data (clj->js (mapv (fn [] {:x (rand-int width) :y (rand-int height)}) (range 100)))
 
-        raw-data (mapv (fn [] {:x (rand-int width) :y (rand-int height)}) (range 100))
-        data (clj->js raw-data)
-
-        svg (d3.select "#voronoi")
+        svg (d3.select node)
 
         voronoi (-> (d3.geom.voronoi)
                     (.x (fn [d] (aget d "x")))
